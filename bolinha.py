d9215e86ddcf1753e8a3e8f16168c781c8c68658
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-11-12 17:17:37 -0300

    pulando colisões complexas para garantir o básico logo

--- bolinha.py
+++ bolinha.py
@@ -20,47 +20,62 @@ class Bolinha( PPlay ):
         self.set_position( x , y )
         
         # metros por segundo
-        self.speed = kwargs.get( "speed" , 10 )
-        
-        # angulo com o eixo x, em radianos
-        self.theta = kwargs.get( "theta" , 0 )  
-        
+        self.vx = kwargs.get( "vx" , 10 )
+        self.vy = kwargs.get( "vy" , 0 )
+         
         # quilogramas
         self.mass  = kwargs.get( "mass" , 1 ) 
     
-    def to_vectorial( self ):
+    def move( self, dt, g = 9.81 ):
 
-        vy = numpy.sin( self.theta )*self.speed
-        vx = numpy.cos( self.theta )*self.speed
-
-        return vx , vy
-
-    def move( self, dt ):
-
-        vx , vy = self.to_vectorial()
-        self.move_y( vy*dt )
-        self.move_x( vx*dt )
-
-    def update_speed( vx , vy ):
-
-        self.speed = numpy.sqrt( vx**2 + vy**2 )
-        self.theta = numpy.arctan( vy/vx )
-
-    def fall( self , dt , g = 9.81 ):
-
-        #g é o coeficiente de acc da gravidade
+        self.move_y( self.vy*dt )
+        self.move_x( self.vx*dt )
  
-        vx , vy = self.to_vectorial()
-        vy = vy - g*dt
-        self.update_speed( vx , vy )
+        self.vy = self.vy - g*dt
     
+    def simple_bounce( self, k = .8 , vertical = False ):
+
+        '''
+        versão mais simplificada. Paredes sempre retas
+        '''
+
+        k = numpy.clip( k, 0. , 1. )
+        if vertical: 
+            self.vy *= -k
+            return
+        self.vx *= -k
+
+
     def bounce( self , **kwargs ):
+        
+        '''
+        alteração do movimento da bola quando quando bate
+        na superficie. No caso geral a barra, teto ou chão.
+        Essa função tem como efeito colateral a mudança 
+        nos valores de vx e vy da bola, e retorna a quantida
+        de de energia dissipada na colisão, para computar o
+        quão alto será o som do impacto. Quando nenhum argumento
+        for passado. Considera-se o chão a superfície.
+        '''
 
         #--------------------------------------------------
-        # é a bola sendo rebatida, reage a uma colisão com
-        # o chão, o teto, o chão ou uma das barras. A função recebe
-        # massa, o vetor velocidade do objeto que colidiu, além do 
-        # coeficiente de restituição. Retorna a quantidade de energia
-        # dissipada.
+        # a massa do objeto que colide. Para fins práticos o chão e 
+        # teto tem massa infinita
+        mass = kwargs.get( 'mass' , sys.maxint )
 
+        #--------------------------------------------------
+        # o angulo que o vetor normal a superfície faz com o eixo
+        # x. Relevante pois planejo fazer a barra dos jogadores
+        # inclinável.
+        norm = kwargs.get( 'norm' , numpy.pi/2 )
+
+        # velocidade da superfície, caso se mova.
+        ux = kwargs.get( 'ux' , 0 )
+        uy = kwargs.get( 'uy' , 0 )
+        
+        #--------------------------------------------------
+        # coeficiente de restituição. Varia de 0 ( Colisão perfeita
+        # mente inelástica ) a 1. ( perfeitamente elástica ). É usada
+        # para calcular a dissipação de energia. 
+        k = kwargs.get( 'k' , .8 )
 
