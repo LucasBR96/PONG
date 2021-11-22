949e5c90f44003b2c674989c26275f20ec57cf2e
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-11-22 09:45:17 -0300

    esqueci de refatorar algumas constantes

--- constantes.py
+++ constantes.py
@@ -11,7 +11,7 @@ SCREEN_H = QUADRA_H*R
 # R = SCREEN_W/QUADRA_W
 
 # Bola
-BOLA_VEL   = 5.
+BOLA_VEL   = 5.              # Modulo da velocidade inicial
 BOLA_THETA = numpy.pi/3
 BOLA_START = numpy.array( [ QUADRA_W , QUADRA_H ] )/2
 BOLA_MASS  = .3
@@ -30,18 +30,15 @@ PAUSE_T = .1
 # Pad
 PAD_TAM    = 1.5
 PAD_SPEED  = 4.
-ROT_SPEED  = 1.
-MAX_THETA  = numpy.pi/3
-RIGHT_NORM = numpy.array( [1. , 0 ] )
+PAD_MASS   = 1.
 RIGHT_PAD  = numpy.array( [ QUADRA_W/4 , QUADRA_H/2 ] )
 RIGHT_BOX  = [ 0 , 0 , QUADRA_W/3 , QUADRA_H ]
-LEFT_NORM  = numpy.array( [-1. , 0 ] )
 LEFT_PAD   = numpy.array( [ 3*QUADRA_W/4 , QUADRA_H/2 ] )
 LEFT_BOX   = [ 2*QUADRA_W/3 , 0 , QUADRA_W/3 , QUADRA_H ]
 
 # Física
-G = 2.81   # Acc da Gravidade
-K = 1.   # Coef de restituição
+G = 9.81   # Acc da Gravidade
+K = 1.     # Coef de restituição
 
 # Imagens
 BOLA_IMG = 'basquete.png'
@@ -61,7 +58,6 @@ FLOOR      = 4
 NO_PAD    = 0
 SIDE_PAD  = 1
 OVR_PAD   = 2 # Tanto em cima como abaixo
-
 
 #Teclado
 RPAD_KEYS = ['w' , 's' , 'a' , 'd' ]
